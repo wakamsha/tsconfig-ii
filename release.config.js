8cff1b166d5e2b502afcd7d6798a4027f4561bcf
YAMADA Naoki <wakamsha@gmail.com>
2024-05-13 15:14:00 +0900

    feat: Setup Node.js structure (#1)
    * setup package.json
    
    * setup node version reqirement
    
    * implement release config
    
    * specify codeowners
    
    * implement gha workflow

---
+++ release.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+        message:
+        "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+    // "@semantic-release/npm",
+  ],
+};
