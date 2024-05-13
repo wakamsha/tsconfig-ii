615fbb107e071abcda4ebb2ca6ec6340eb9e9eb6
wakamsha <wakamsha@gmail.com>
2024-05-13 16:34:26 +0900

    chore: fix release config

--- release.config.js
+++ release.config.js
@@ -7,15 +7,15 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    // "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
         message:
-        "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
+          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
       },
     ],
     "@semantic-release/github",
-    // "@semantic-release/npm",
   ],
 };
