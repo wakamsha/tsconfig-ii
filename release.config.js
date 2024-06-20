6def90f6a22ff3c9039738794adf1343981db20a
YAMADA Naoki <wakamsha@gmail.com>
2024-06-20 16:21:43 +0900

    ci: Insert pr step into release workflow (#9)
    * remove unnecessary fields
    
    * install package

--- release.config.js
+++ release.config.js
@@ -2,20 +2,23 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
     [
-      "@semantic-release/git",
+      "semantic-release-github-pullrequest",
       {
         assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        message:
-          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
       },
     ],
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+    //   },
+    // ],
     "@semantic-release/github",
   ],
 };
